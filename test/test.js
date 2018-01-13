1dea9f64e99425d8834eae8b81072137246e1ecd
Sharuk Ahmed Mazumder <sharuk.ahmed@coda.global>
2018-01-14 00:43:30 +0530

    Got all the links

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+
+const index = require('../index');
+
+describe('Sanity check', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+describe('Sanity check2', function() {
+    describe('#indexOf()', function() {
+      it('should return -1 when the value is not present', function(){
+         new index().crawler();
+        assert.equal(true, true);
+      });
+    });
+  });
