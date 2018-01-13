19f5922d539637112d4d017c03bc1afb6f1f8f31
Sharuk Ahmed Mazumder <sharuk.ahmed@coda.global>
2018-01-14 02:01:54 +0530

    added seed url

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ describe('Sanity check', function() {
 describe('Sanity check2', function() {
     describe('#indexOf()', function() {
       it('should return -1 when the value is not present', function(){
-         new index().crawler();
+         new index().crawler("https://www.npmjs.com");
         assert.equal(true, true);
       });
     });
