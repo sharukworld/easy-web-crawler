a84544b732ef831cb9d78e338e024ceeb9d75779
Sharuk Ahmed Mazumder <sharuk.ahmed@coda.global>
2018-01-14 23:38:14 +0530

    first iteration completed for the seeded url

--- constant/ApplicationConstant.js
+++ constant/ApplicationConstant.js
@@ -6,5 +6,6 @@ module.exports = Object.freeze({
     VISITED_STORE_NAME: "visited_set_",
     TO_BE_VISITED_STORE_NAME: "to_ve_visited_sorted_set_",
     SCORE_INCREMENT_VALUE: 1,
-    CHECK_IF_URL_SEEDED: "SISMEMBER"
+    CHECK_IF_URL_SEEDED: "SISMEMBER",
+    INCR_SCORE: "ZINCRBY"
 });
--- util/link-util.js
+++ util/link-util.js
@@ -2,23 +2,33 @@ let url = require("url");
 var ApplicationConstant = require('../constant/ApplicationConstant');
 
 exports.getCompleteLink = function (link, parentLink) {
-    if(link == null){
+    if (link == null) {
         return;
     }
     let parsedLink = url.parse(link);
-    if(parsedLink.hostname == null){
+    if (parsedLink.hostname == null) {
         let parsedParentLink = url.parse(parentLink);
-        return parsedParentLink.protocol+ "//"+ parsedParentLink.hostname + link;
+        return parsedParentLink.protocol + "//" + parsedParentLink.hostname + link;
     }
     return link;
 }
 
 exports.getIsLinkSeededTransactionArray = function (linkArray, seedUrl) {
-    console
     let linkSeededTransactionArray = [];
     let vistedStoreName = ApplicationConstant.VISITED_STORE_NAME + seedUrl;
-    for(link in linkArray){
-        linkSeededTransactionArray.push([ApplicationConstant.CHECK_IF_URL_SEEDED, vistedStoreName, link]);
+    for (let index = 0; index < linkArray.length; index++) {
+        linkSeededTransactionArray.push([ApplicationConstant.CHECK_IF_URL_SEEDED, vistedStoreName, linkArray[index]]);
     }
-   return linkSeededTransactionArray;
+    return linkSeededTransactionArray;
+}
+
+exports.getScoreIncrArray = function (linkArray, isScrapped, seedUrl) {
+    let scoreIncrArray = [];
+    let storeName = ApplicationConstant.TO_BE_VISITED_STORE_NAME + seedUrl;
+   for(let index = 0; index < linkArray.length; index++) {
+     if(isScrapped[index] === 0) {
+        scoreIncrArray.push([ApplicationConstant.INCR_SCORE, storeName, ApplicationConstant.SCORE_INCREMENT_VALUE, linkArray[index]]);
+     }
+   }
+   return scoreIncrArray;
 }
