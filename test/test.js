592a465e528aca0850724a3a3d6ed4df41dd4a82
Sharuk Ahmed Mazumder <sharuk.ahmed@coda.global>
2018-01-15 01:10:34 +0530

    completed first iteration

--- test/test.js
+++ test/test.js
@@ -4,18 +4,30 @@ const index = require('../index');
 var redis = require('../util/redis-db');
 let db = redis.connect();
 
-let crawl = new index("https://stackoverflow.com", db);
+let crawl = new index("http://google.com", db, 10000);
 
 
 describe('Sanity check2', function() {
     describe('#indexOf()', function() {
       it('should return -1 when the value is not present', function(){
-        crawl.startCrawling();
+        crawl.startCrawling().then(
+          list => {
+            console.log("inside test", list);
+          }
+        );
         assert.equal(true, true);
       });
     });
   });
 
+  // describe('Sanity check2', function() {
+  //   describe('get highest scored url', function() {
+  //     it('get highest scored url', function(){
+  //       crawl.popHighestScoredUrl();
+  //       assert.equal(true, true);
+  //     });
+  //   });
+  // });
   // describe('check upserting of link', function() {
   //   describe('#indexOf()', function() {
   //     it('should add a link', function(){
